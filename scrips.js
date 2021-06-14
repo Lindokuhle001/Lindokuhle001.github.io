b06a8d5409178642c63a0a94f5b06ea3833b77a2
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-06-14 09:54:16 +0200

    put files inside folders

--- scrips.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle_btn')[0];
-const navbarLinks = document.getElementsByClassName ('navbar_links')[0];
-const navbar = document.getElementsByClassName('navbar')[0];
-
-toggleButton.addEventListener('click',() => {
-    navbarLinks.classList.toggle('active');
-    navbar.classList.toggle('on');
-});
