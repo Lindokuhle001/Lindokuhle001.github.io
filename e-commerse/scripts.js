d088e15b47b1e21759a0f2b45d8d0e4d8e39fe63
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-07-06 13:37:43 +0200

    added e commerse link

---
+++ e-commerse/scripts.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle_btn')[0];
+const navbarLinks = document.getElementsByClassName ('navbar_links')[0];
+const navbar = document.getElementsByClassName('navbar')[0];
+
+toggleButton.addEventListener('click',() => {
+    navbarLinks.classList.toggle('active');
+    navbar.classList.toggle('on');
+});
+
+const removeCartItem = document.getElementsByClassName('btn-danger')[0];
